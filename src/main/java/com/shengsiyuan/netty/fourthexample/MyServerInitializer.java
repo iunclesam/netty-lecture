9f39bac78c82ff854df767782546a5f66c143d38
Your Name <you@example.com>
2019-10-07 23:28:57 +0800

    心跳

---
+++ src/main/java/com/shengsiyuan/netty/fourthexample/MyServerInitializer.java
@@ -0,0 +1,23 @@
+package com.shengsiyuan.netty.fourthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
+import io.netty.util.CharsetUtil;
+
+import java.util.concurrent.TimeUnit;
+
+public class MyServerInitializer extends ChannelInitializer<SocketChannel>{
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new IdleStateHandler(5,7,3,TimeUnit.SECONDS));
+        pipeline.addLast(new MyServerHandler());
+    }
+}
