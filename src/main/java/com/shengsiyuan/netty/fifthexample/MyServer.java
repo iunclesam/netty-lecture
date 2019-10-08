5bc93545dd0394d6e89630cb7d537d37cf5e98b1
Your Name <you@example.com>
2019-10-08 23:52:34 +0800

    websocket

--- src/main/java/com/shengsiyuan/netty/fifthexample/MyServer.java
+++ src/main/java/com/shengsiyuan/netty/fifthexample/MyServer.java
@@ -1,13 +1,18 @@
-package com.shengsiyuan.netty.secondexample;
+package com.shengsiyuan.netty.fifthexample;
+
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.net.InetSocketAddress;
 
 public class MyServer {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
@@ -15,8 +20,9 @@ public class MyServer {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
-                    .childHandler(new MyServerInitializer());
-            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new WebSocketChannelInitailizer());
+            ChannelFuture channelFuture = serverBootstrap.bind(new InetSocketAddress(8899)).sync();
             channelFuture.channel().closeFuture().sync();
         } finally {
             bossGroup.shutdownGracefully();
